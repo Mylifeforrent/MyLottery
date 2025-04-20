914c0b2172b9555482d5bbc9c0f374616238d7f9
Tony <1299351443@qq.com>
2025-04-20 22:11:21 +0800

    feature/20250420_strategy: submit code for strategy implementation

--- lottery-interfaces/src/main/resources/db/migration/V1__init.sql
+++ lottery-interfaces/src/main/resources/db/migration/V1__init.sql
@@ -20,25 +20,32 @@
 -- ----------------------------
 -- Table structure for activity
 -- ----------------------------
+create database lottery;
 DROP TABLE IF EXISTS `activity`;
-CREATE TABLE `activity` (
-                            `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-                            `activity_id` bigint(20) NOT NULL COMMENT '活动ID',
-                            `activity_name` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '活动名称',
-                            `activity_desc` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '活动描述',
-                            `begin_date_time` datetime DEFAULT NULL COMMENT '开始时间',
-                            `end_date_time` datetime DEFAULT NULL COMMENT '结束时间',
-                            `stock_count` int(11) DEFAULT NULL COMMENT '库存',
-                            `take_count` int(11) DEFAULT NULL COMMENT '每人可参与次数',
-                            `state` tinyint(2) DEFAULT NULL COMMENT '活动状态：1编辑、2提审、3撤审、4通过、5运行(审核通过后worker扫描状态)、6拒绝、7关闭、8开启',
-                            `creator` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '创建人',
-                            `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-                            `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-                            PRIMARY KEY (`id`),
-                            UNIQUE KEY `unique_activity_id` (`activity_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='活动配置';
 
+-- auto-generated definition
+create table activity
+(
+    id            bigint auto_increment comment '自增ID',
+    activityId    bigint       null comment '活动ID',
+    activityName  varchar(64)  not null comment '活动名称',
+    activityDesc  varchar(128) null comment '活动描述',
+    beginDateTime datetime     not null comment '开始时间',
+    endDateTime   datetime     not null comment '结束时间',
+    stockCount    int          not null comment '库存',
+    takeCount     int          null comment '每人可参与次数',
+    state         int          null comment '活动状态：编辑、提审、撤审、通过、运行、拒绝、关闭、开启',
+    creator       varchar(64)  not null comment '创建人',
+    createTime    datetime     not null comment '创建时间',
+    updateTime    datetime     not null comment '修改时间',
+    constraint activity_id_uindex
+        unique (id)
+)
+    comment '活动配置';
+
+alter table activity
+    add primary key (id);
 
 -- 插入测试数据
-INSERT INTO activity (id,activity_id, activity_name, activity_desc, begin_date_time, end_date_time, stock_count, take_count, state, creator)
+INSERT INTO activity (id,activityId, activityName, activityDesc, beginDateTime, endDateTime, stockCount, takeCount, state, creator)
 VALUES (100001,100001, 'Test Activity', 'This is a test activity', '2023-01-01 00:00:00', '2023-12-31 23:59:59', 100, 1, 1, 'admin');
