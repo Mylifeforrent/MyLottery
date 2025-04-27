eff91884a250646479970ceaedfaff84d2e40eda
Tony <1299351443@qq.com>
2025-04-27 09:31:44 +0800

    feature/20250429_ApplicationActivityProcess: implement activity draw process in application module

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/model/req/DrawReq.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/model/req/DrawReq.java
@@ -1,15 +1,41 @@
 package cn.itedus.lottery.domain.strategy.model.req;
 
-
+/**
+ * @description: 抽奖请求对象
+ * @author: 小傅哥，微信：fustack
+ * @date: 2021/9/4
+ * @github: https://github.com/fuzhengwei
+ * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
 public class DrawReq {
 
+    /**
+     * 用户ID
+     */
     private String uId;
 
+    /**
+     * 策略ID
+     */
     private Long strategyId;
+
+    /**
+     * 防重ID
+     */
+    private String uuid;
+
+    public DrawReq() {
+    }
 
     public DrawReq(String uId, Long strategyId) {
         this.uId = uId;
         this.strategyId = strategyId;
+    }
+
+    public DrawReq(String uId, Long strategyId, String uuid) {
+        this.uId = uId;
+        this.strategyId = strategyId;
+        this.uuid = uuid;
     }
 
     public String getuId() {
@@ -27,4 +53,13 @@ public class DrawReq {
     public void setStrategyId(Long strategyId) {
         this.strategyId = strategyId;
     }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
 }
